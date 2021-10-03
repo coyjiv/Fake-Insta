5f662444a01982191ce66604985e00eab29fafef
Даниил Щербаков <djdaniil46@gmail.com>
2021-10-03 17:32:07 +0300

    new posts

--- client/src/components/feature/VisitPostsWrapper/VisitPostsWrapper.jsx
+++ client/src/components/feature/VisitPostsWrapper/VisitPostsWrapper.jsx
@@ -2,18 +2,14 @@ import React, {useEffect} from 'react';
 import {getPosts} from "../../../store/visited_page/operations";
 import SmallPost from "./SmallPost/SmallPost";
 import styles from "./VisitPostsWrapper.module.scss";
+import ProfilePostShortcut from "../ProfilePostShortcut/ProfilePostShortcut";
 
 const VisitPostsWrapper = (props) => {
     const postCol=props.posts;
     return(
     <div className={styles.wrapper}>
-        {typeof props.posts[0]==="object"? props.posts.map((el)=><SmallPost
-            description={el.description}
-            src={el.image}
-            likes={el.likes}
-            comments={el.comments}
-            id={el._id}
-            imgwidth={props.imgwidth}
+        {typeof props.posts[0]==="object"? props.posts.map((el)=><ProfilePostShortcut
+            post={el}
             key={el._id}
         />):null}
         </div>
